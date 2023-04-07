ac4ffa6f8692bc7e6c1b304d239a3160a589069c
trevin.livele <liveletrevin6@gmail.com>
2023-04-07 13:46:20 +0300

    refactored the expense items

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -44,7 +44,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -79,14 +79,15 @@ const ExpenseForm = (props) => {
           <label>Date</label>
           <input
             type='date'
-            min='2019-01-01'
-            max='2022-12-31'
+            min='2000-01-01'
+            max='2025-12-31'
             value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
